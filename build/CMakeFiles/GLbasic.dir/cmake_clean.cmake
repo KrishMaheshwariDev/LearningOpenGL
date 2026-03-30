cf86bb549051098164e91b90d24a132ab68766c4
KrishMaheshwariDev <krishmaheshwari197903@gmail.com>
2026-03-30 12:22:37 +0530

    just setting up dependencies

---
+++ build/CMakeFiles/GLbasic.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GLbasic.dir/libs/glad/src/glad.c.obj"
+  "CMakeFiles/GLbasic.dir/libs/glad/src/glad.c.obj.d"
+  "CMakeFiles/GLbasic.dir/src/main.cpp.obj"
+  "CMakeFiles/GLbasic.dir/src/main.cpp.obj.d"
+  "GLbasic.exe"
+  "GLbasic.exe.manifest"
+  "GLbasic.pdb"
+  "libGLbasic.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/GLbasic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/Main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+static void error_callback(int error, const char* description) {
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+int main() {
+	// initializing the glfw to use its features
+	if (!glfwInit()) {
+		return -1;
+	}
+	// Telling GLFW about the context
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Basic", NULL, NULL);
+	if (window == NULL) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// terminating the glfw
+	glfwTerminate();
+	return 0;
+}
+
+
+//static void key_callback()
